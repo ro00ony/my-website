84cf196e0ab59826db50b1d12428b53b4da147d2
Abdelrahman Badr <rayanroony2000@gmail.com>
2024-11-19 17:31:38 +0200

    Make some adjustments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1092,7 +1092,4 @@ jQuery(function ($) {
         callback: callbackFunction
     });
 
-	// Switch Btn
-	$('body').append("<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>");
-
 }($));
